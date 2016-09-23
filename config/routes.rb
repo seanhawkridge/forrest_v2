97289fec508a8c4c5adf1ca3ebb4573223a5cb23
Sean Hawkridge <sean.hawkridge@alliants.com>
2016-09-23 16:53:38 +0100

    FOR-12 Header Nav

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :tournaments
   resources :matches
   resources :players
-  root to: 'visitors#index'
+  root to: 'players#index'
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signin' => 'sessions#new', :as => :signin
   get '/signout' => 'sessions#destroy', :as => :signout
