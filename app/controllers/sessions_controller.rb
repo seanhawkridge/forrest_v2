90fc757103612a4fd9680c720dac46a4b4532d98
Sean Hawkridge <sean.hawkridge@alliants.com>
2016-10-12 16:20:42 +0100

    removing alerts for sign in and out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,12 +10,12 @@ class SessionsController < ApplicationController
                       :uid => auth['uid'].to_s).first || User.create_with_omniauth(auth)
     reset_session
     session[:user_id] = user.id
-    redirect_to root_url, :notice => 'Signed in!'
+    redirect_to root_url
   end
 
   def destroy
     reset_session
-    redirect_to root_url, :notice => 'Signed out!'
+    redirect_to root_url
   end
 
   def failure
