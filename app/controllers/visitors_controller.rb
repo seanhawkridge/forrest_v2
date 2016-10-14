0d6429ab0f875d0d931cc3af4b2e6f848c52a6c1
Sean Hawkridge <sean.hawkridge@alliants.com>
2016-10-14 12:15:44 +0100

    adding points to homepage

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,7 +2,7 @@ class VisitorsController < ApplicationController
 
   def index
     @matches = Match.with_results.limit(3)
-    @players = Player.by_win_percantage.limit(4)
+    @players = Player.by_points.limit(4)
   end
 
 end
