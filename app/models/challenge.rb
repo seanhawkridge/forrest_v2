ac47a33b0b9b8adc8f1cafae42eaa5da3b8a683e
Sean Hawkridge <sean.hawkridge@alliants.com>
2016-10-26 13:32:33 +0100

    fixing bug in challenge player scope

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -4,8 +4,8 @@ class Challenge < ApplicationRecord
   belongs_to :challenged, class_name: 'Player'
   belongs_to :winner, class_name: 'Player', optional: true
 
-  scope :includes_player, -> (player) { where("status = ? AND (challenger_id = ? OR challenged_id = ?)",
-                                       "open", player.id, player.id) }
+  scope :includes_player, -> (player) { where("(status = ? OR status = ?) AND (challenger_id = ? OR challenged_id = ?)",
+                                       "open", "accepted", player.id, player.id) }
 
   def open_challenge?
     status == "open"
